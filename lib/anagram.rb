b69dda5683c8459bc681f66c2446b563851c01e2
Thor Summerall <thor.summerall@gmail.com>
2020-12-05 13:18:39 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :anagrams
+
+  def initialize(anagrams)
+    @anagrams = anagrams
+  end
+
+  def match
+    # needs to iterate over array
+    # find matches
+    # return new array with matches
+    # if no match, return false
+  end
+end
